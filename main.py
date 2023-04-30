5f70ab407db2a4de2fc20b0141d29ceecc81c47b
Josh B <joshlegs@gmail.com>
2023-04-30 15:37:19 -0400

    fix: idk im flailing

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def init():
 
     print(f"You said: {speech}")
     # Load the model and processor
-    # model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
-    # processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
+    model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
+    processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
 
     # # Read the audio file
     # audio, sample_rate = sf.read("path/to/your/audio/file.wav")
@@ -47,17 +47,17 @@ def init():
     #     audio = resample_poly(audio, 16000, sample_rate)
 
     # # Tokenize the audio
-    # input_values = processor(audio, return_tensors="tf", padding="longest").input_values
+    input_values = processor(speech, return_tensors="tf", padding="longest").input_values
 
     # # Perform speech recognition
-    # logits = model(input_values).logits
+    logits = model(input_values).logits
 
     # # Decode the logits to text
-    # predicted_ids = tf.argmax(logits, axis=-1).numpy()
-    # transcription = processor.batch_decode(predicted_ids)[0]
+    predicted_ids = tf.argmax(logits, axis=-1).numpy()
+    transcription = processor.batch_decode(predicted_ids)[0]
 
     # # Print the transcription
-    # print(transcription)
+    print(transcription)
 
 if __name__ == "__main__":
     init()
