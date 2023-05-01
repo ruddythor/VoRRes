0a7d84b198f493563e9941c2c840c267b624ba63
Josh B <joshlegs@gmail.com>
2023-05-01 19:27:46 -0400

    fix: start over from the beginning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pyaudio
 import numpy as np
 import soundfile as sf
-import requests
-import json
-import pyttsx3
 from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor
 from scipy.signal import resample_poly
 
@@ -20,79 +17,35 @@ def transcribe_audio(audio):
     transcription = processor.batch_decode(predicted_ids)[0]
     return transcription
 
-# Function to send text to ChatGPT and get the response
-def chat_with_gpt(text):
-    api_key = "your_openai_api_key"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}",
-    }
-    data = json.dumps({
-        "model": "text-davinci-002",
-        "prompt": text,
-        "temperature": 0.7,
-        "max_tokens": 100,
-    })
-    response = requests.post("https://api.openai.com/v1/engines/davinci-codex/completions", headers=headers, data=data)
-    response_text = response.json()["choices"][0]["text"].strip()
-    return response_text
-
-# Function to convert text to speech
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
 # Record audio from the microphone
-import pyaudio
-import wave
-
-def record_audio_to_file(filename, record_seconds=5):
+def record_audio(record_seconds=5):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
-
     p = pyaudio.PyAudio()
-
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
-
     print("Recording...")
-
     frames = []
-
     for _ in range(0, int(RATE / CHUNK * record_seconds)):
         data = stream.read(CHUNK)
-        frames.append(data)
-
+        frames.append(np.frombuffer(data, dtype=np.int16))
     print("Finished recording.")
-
     stream.stop_stream()
     stream.close()
     p.terminate()
-
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    return np.hstack(frames)
 
 # Main function
 def main():
     while True:
-        audio = record_audio_to_file("output.wav", record_seconds=5)
-
+        audio = record_audio()
         transcription = transcribe_audio(audio)
         print("You said:", transcription)
-        response_text = chat_with_gpt(transcription)
-        print("ChatGPT says:", response_text)
-        text_to_speech(response_text)
 
 if __name__ == "__main__":
     main()
-
