9564d7644984a7e5420930c7f1323bef944bafdd
Josh B <joshlegs@gmail.com>
2023-05-01 19:41:13 -0400

    fix: start over again, using speechrecognition library

--- main.py
+++ main.py
@@ -1,51 +1,26 @@
-import pyaudio
-import numpy as np
-import soundfile as sf
-from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor
-from scipy.signal import resample_poly
+import speech_recognition as sr
 
-# Initialize ASR model
-model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
-processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
-
-# Function to transcribe audio using the ASR model
-def transcribe_audio(audio):
-    audio = resample_poly(audio, 16000, 44100)
-    input_values = processor(audio, return_tensors="tf", padding="longest").input_values
-    logits = model(input_values).logits
-    predicted_ids = np.argmax(logits, axis=-1)
-    transcription = processor.batch_decode(predicted_ids)[0]
-    return transcription
-
-# Record audio from the microphone
-def record_audio(record_seconds=5):
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-    print("Recording...")
-    frames = []
-    for _ in range(0, int(RATE / CHUNK * record_seconds)):
-        data = stream.read(CHUNK)
-        frames.append(np.frombuffer(data, dtype=np.int16))
-    print("Finished recording.")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    return np.hstack(frames)
+# Function to transcribe audio using the SpeechRecognition package
+def transcribe_audio(recognizer, audio):
+    try:
+        transcription = recognizer.recognize_google(audio)
+        return transcription
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand the audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # Main function
 def main():
+    recognizer = sr.Recognizer()
     while True:
-        audio = record_audio()
-        transcription = transcribe_audio(audio)
-        print("You said:", transcription)
+        with sr.Microphone() as source:
+            print("Recording...")
+            audio = recognizer.listen(source)
+            print("Finished recording.")
+            transcription = transcribe_audio(recognizer, audio)
+            if transcription:
+                print("You said:", transcription)
 
 if __name__ == "__main__":
     main()
