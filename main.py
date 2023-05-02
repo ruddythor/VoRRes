091aec0a16b3224d00a39fbc37bb4151dbdadd23
Josh B <joshlegs@gmail.com>
2023-05-02 16:10:58 -0400

    fix: push changes so i dont lose them

--- main.py
+++ main.py
@@ -1,76 +1,81 @@
-import speech_recognition as sr
-import pyaudio
 import numpy as np
+import sounddevice as sd
 import soundfile as sf
-import pyttsx3
-from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor, GPT2LMHeadModel, GPT2Tokenizer
-from scipy.signal import resample_poly
+import samplerate as sr
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+# from datasets import load_dataset
+import torch
+ 
+ # load model and tokenizer
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+     
+ # load dummy dataset and read soundfiles
+# ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+ 
+
+
+
 
 # Initialize ASR model
-print("Loading and initializing ASR model ...")
-asr_model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
-asr_processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
-
-# Initialize GPT model
-print("Loading and initializing GPT model ...")
-gpt_model = GPT2LMHeadModel.from_pretrained("gpt2")
-gpt_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# asr_model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
+# asr_processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
 
 # Function to transcribe audio using the ASR model
 def transcribe_audio(audio):
-    audio = resample_poly(audio, 16000, 44100)
-    input_values = asr_processor(audio, return_tensors="tf", padding="longest").input_values
+    audio = sr.resample(audio, 1.5, 'sinc_best')
+    asr_proc = asr_processor(audio, return_tensors="tf", padding="longest", sampling_rate=16000)
+    print(asr_proc)
+    input_values = asr_proc.input_values
     logits = asr_model(input_values).logits
     predicted_ids = np.argmax(logits, axis=-1)
     transcription = asr_processor.batch_decode(predicted_ids)[0]
     return transcription
 
-# Function to send a message to GPT and receive a response
-def chat_with_gpt(text):
-    input_ids = gpt_tokenizer.encode(f"{text}", return_tensors="tf")
-    output = gpt_model.generate(input_ids, max_length=150, num_return_sequences=1)
-    response_text = gpt_tokenizer.decode(output[0], skip_special_tokens=True)
-    response_text = response_text.split("AI:")[-1].strip()
-    return response_text
-
-# Function to convert text to speech
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
 # Record audio from the microphone
 def record_audio(record_seconds=5):
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
     RATE = 44100
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+    CHANNELS = 1
     print("Recording...")
-    frames = []
-    for _ in range(0, int(RATE / CHUNK * record_seconds)):
-        data = stream.read(CHUNK)
-        frames.append(np.frombuffer(data, dtype=np.int16))
+    audio = sd.rec(int(record_seconds * RATE), samplerate=RATE, channels=CHANNELS, dtype='float32')
+    sd.wait()
     print("Finished recording.")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    return np.hstack(frames)
+    return audio.flatten()
+
+# Save audio to a WAV file
+def save_audio_to_wav(audio, filename):
+    sf.write(filename, audio, 44100)
+
+# Load audio from a WAV file
+def load_audio_from_wav(filename):
+    audio, samplerate = sf.read(filename, dtype='float32')
+    return audio.flatten()
+
+def play_audio(audio):
+    RATE = 44100
+    print("Playing audio...")
+    sd.play(audio, samplerate=RATE)
+    sd.wait()
+    print("Finished playing audio.")
 
 # Main function
 def main():
-    while True:
-        audio = record_audio()
-        transcription = transcribe_audio(audio)
-        print("You said:", transcription)
-        response_text = chat_with_gpt(transcription)
-        print("AI says:", response_text)
-        text_to_speech(response_text)
+    audio = record_audio()
+    save_audio_to_wav(audio, 'audio_input.wav')
+    audio_from_wav = load_audio_from_wav('audio_input.wav')
+    play_audio(audio_from_wav)    
+    # transcription = transcribe_audio(audio_from_wav)
+     # tokenize
+    input_values = processor(audio_from_wav, return_tensors="pt", padding="longest", sampling_rate=16000).input_values  # Batch size 1
+    
+    # retrieve logits
+    logits = model(input_values).logits
+    
+    # take argmax and decode
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = processor.batch_decode(predicted_ids)
+
+    print("Transcription:", transcription)
 
 if __name__ == "__main__":
     main()
