dfabc63746c436d6cee9193c45c1ad2cdcb12146
Josh B <joshlegs@gmail.com>
2023-05-02 21:56:09 -0400

    fix: holy smokes i think it worked

--- main.py
+++ main.py
@@ -1,80 +1,56 @@
-import numpy as np
 import sounddevice as sd
-import soundfile as sf
-import samplerate as sr
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-# from datasets import load_dataset
+import numpy as np
+from scipy.io.wavfile import write, read
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import torch
- 
- # load model and tokenizer
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-     
- # load dummy dataset and read soundfiles
-# ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
- 
 
-
-
-
-# Initialize ASR model
-# asr_model = TFWav2Vec2ForCTC.from_pretrained("openai/whisper-tiny")
-# asr_processor = Wav2Vec2Processor.from_pretrained("openai/whisper-tiny")
-
-# Function to transcribe audio using the ASR model
-def transcribe_audio(audio):
-    audio = sr.resample(audio, 1.5, 'sinc_best')
-    asr_proc = asr_processor(audio, return_tensors="tf", padding="longest", sampling_rate=16000)
-    print(asr_proc)
-    input_values = asr_proc.input_values
-    logits = asr_model(input_values).logits
-    predicted_ids = np.argmax(logits, axis=-1)
-    transcription = asr_processor.batch_decode(predicted_ids)[0]
-    return transcription
+RECORD_SECONDS = 5
+SAMPLE_RATE = 16000
 
 # Record audio from the microphone
-def record_audio(record_seconds=5):
-    RATE = 44100
-    CHANNELS = 1
+def record_audio():
     print("Recording...")
-    audio = sd.rec(int(record_seconds * RATE), samplerate=RATE, channels=CHANNELS, dtype='float32')
+    audio = sd.rec(int(RECORD_SECONDS * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1)
     sd.wait()
-    print("Finished recording.")
-    return audio.flatten()
+    return audio
 
-# Save audio to a WAV file
-def save_audio_to_wav(audio, filename):
-    sf.write(filename, audio, 44100)
+# Save audio to a file
+def save_audio_to_file(filename, audio):
+    write(filename, SAMPLE_RATE, audio)
 
-# Load audio from a WAV file
-def load_audio_from_wav(filename):
-    audio, samplerate = sf.read(filename, dtype='float32')
-    return audio.flatten()
-
-def play_audio(audio):
-    RATE = 44100
-    print("Playing audio...")
-    sd.play(audio, samplerate=RATE)
+# Play audio from a file
+def play_audio_from_file(filename):
+    print("Playing...")
+    sample_rate, audio = read(filename)
+    sd.play(audio, sample_rate)
     sd.wait()
-    print("Finished playing audio.")
 
-# Main function
+# Transcribe audio file using Wav2Vec2
+def transcribe_audio(filename):
+    # Load pre-trained model and tokenizer
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+    # Load audio
+    sample_rate, audio = read(filename)
+    
+    # Preprocess audio
+    input_values = processor(audio, sampling_rate=sample_rate, return_tensors="pt").input_values
+    
+    # Perform transcription
+    with torch.no_grad():
+        logits = model(input_values).logits
+    predicted_ids = torch.argmax(logits, dim=-1)
+    
+    # Decode the transcription
+    transcription = processor.decode(predicted_ids[0])
+    return transcription
+
 def main():
     audio = record_audio()
-    save_audio_to_wav(audio, 'audio_input.wav')
-    audio_from_wav = load_audio_from_wav('audio_input.wav')
-    play_audio(audio_from_wav)    
-    # transcription = transcribe_audio(audio_from_wav)
-     # tokenize
-    input_values = processor(audio_from_wav, return_tensors="pt", padding="longest", sampling_rate=16000).input_values  # Batch size 1
-    
-    # retrieve logits
-    logits = model(input_values).logits
-    
-    # take argmax and decode
-    predicted_ids = torch.argmax(logits, dim=-1)
-    transcription = processor.batch_decode(predicted_ids)
-
+    save_audio_to_file("recorded_audio.wav", audio)
+    play_audio_from_file("recorded_audio.wav")
+    transcription = transcribe_audio("recorded_audio.wav")
     print("Transcription:", transcription)
 
 if __name__ == "__main__":
