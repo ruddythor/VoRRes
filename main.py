2bd8930362645ba59c8d676576f8740d8a09d4fe
Josh B <joshlegs@gmail.com>
2023-05-02 22:10:34 -0400

    fix: works even better now, with tensorflow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sounddevice as sd
 import numpy as np
 from scipy.io.wavfile import write, read
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
-import torch
+from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor
+import tensorflow as tf
 
 RECORD_SECONDS = 5
 SAMPLE_RATE = 16000
@@ -29,21 +29,20 @@ def play_audio_from_file(filename):
 def transcribe_audio(filename):
     # Load pre-trained model and tokenizer
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    model = TFWav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
     # Load audio
     sample_rate, audio = read(filename)
     
     # Preprocess audio
-    input_values = processor(audio, sampling_rate=sample_rate, return_tensors="pt").input_values
+    input_values = processor(audio, sampling_rate=sample_rate, return_tensors="tf").input_values
     
     # Perform transcription
-    with torch.no_grad():
-        logits = model(input_values).logits
-    predicted_ids = torch.argmax(logits, dim=-1)
+    logits = model(input_values).logits
+    predicted_ids = tf.argmax(logits, axis=-1)
     
     # Decode the transcription
-    transcription = processor.decode(predicted_ids[0])
+    transcription = processor.decode(predicted_ids[0].numpy())
     return transcription
 
 def main():
