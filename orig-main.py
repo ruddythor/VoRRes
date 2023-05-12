07a829ff72c2671694d92e00a51f51be3bdab71e
Josh B <joshlegs@gmail.com>
2023-05-12 19:34:17 -0400

    feat: v0.9! working robot on RasPi!

--- orig-main.py
+++ orig-main.py
@@ -1,17 +1,46 @@
+import sounddevice as sd
 import numpy as np
-from transformers import SpeechT5HifiGan
+from scipy.io.wavfile import write, read
+from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor, SpeechT5HifiGan
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, SpeechT5Processor
+import tensorflow as tf
 import torch
-from transformers import GPT2Tokenizer, SpeechT5ForTextToSpeech, SpeechT5Processor 
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import openai
-
+from transformers import AutoProcessor, SpeechT5ForTextToSpeech
 import soundfile as sf
 from datasets import load_dataset
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
+from transformers import TFWav2Vec2ForCTC, Wav2Vec2Processor, GPT2Tokenizer, TFGPT2LMHeadModel
+import tensorflow as tf
 
 
 RECORD_SECONDS = 5
 SAMPLE_RATE = 16000
 
+def record_audio():
+    print("Recording...")
+    audio = sd.rec(int(RECORD_SECONDS * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1)
+    sd.wait()
+    return audio
+
+def save_audio_to_file(filename, audio):
+    write(filename, SAMPLE_RATE, audio)
+
+def play_audio_from_file(filename):
+    print("Playing...")
+    sample_rate, audio = read(filename)
+    sd.play(audio, sample_rate)
+    sd.wait()
+
+def transcribe_audio(filename):
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    model = TFWav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    sample_rate, audio = read(filename)
+    input_values = processor(audio, sampling_rate=sample_rate, return_tensors="tf").input_values
+    logits = model(input_values).logits
+    predicted_ids = tf.argmax(logits, axis=-1)
+    transcription = processor.decode(predicted_ids[0].numpy())
+    return transcription
 
 def speak_response(response):
     processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
@@ -43,11 +72,10 @@ def generate_response(prompt):
     return response
 
 def main():
-    # audio = record_audio()
-    # save_audio_to_file("recorded_audio.wav", audio)
+    audio = record_audio()
+    save_audio_to_file("recorded_audio.wav", audio)
     # play_audio_from_file("recorded_audio.wav")
-    #transcription = "What is the future of space travel?"
-    transcription = "How can a ship fly using antigravity?"
+    transcription = transcribe_audio("recorded_audio.wav")
     print("Transcription:", transcription)
     response = generate_response(transcription)
     print("Hank says:", response)
